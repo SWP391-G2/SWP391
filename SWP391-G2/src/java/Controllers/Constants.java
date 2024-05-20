ce98f4bb0e2c64c09630f82f78813a5e3a62a65a
Nguyen Quang Bich HE173220 <bichnqhe173220@fpt.edu.vn>
2024-05-21 00:12:52 +0700

    fix error login with google

--- SWP391-G2/src/java/Controllers/Constants.java
+++ SWP391-G2/src/java/Controllers/Constants.java
@@ -15,7 +15,7 @@ public class Constants {
 
 	public static String GOOGLE_CLIENT_SECRET = "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";
 
-	public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/SWP391/LoginGoogleHandler";
+	public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/SWP391-G2/LoginGoogleHandler";
 
 	public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
 
