a2a2cc5316ab99d53c274f5bc5aef3d7e1031448
Nguyen Quang Bich HE173220 <bichnqhe173220@fpt.edu.vn>
2024-05-30 18:26:58 +0700

    fix product.jsp

--- SWP391-G2/src/java/Dal/DBContext.java
+++ SWP391-G2/src/java/Dal/DBContext.java
@@ -23,15 +23,8 @@ public class DBContext {
             //Change the username password and url to connect your own database
             String username = "sa";
             String password = "123";
-<<<<<<< HEAD
-<<<<<<< HEAD
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=TPS23";
-=======
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=DEMO4";
->>>>>>> hoang
-=======
+
             String url = "jdbc:sqlserver://localhost:1433;databaseName=TPS";
->>>>>>> e618cb242a2e22b3024d1cc5db33b9c9d1aae8f3
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, username, password);
         } catch (ClassNotFoundException | SQLException ex) {
