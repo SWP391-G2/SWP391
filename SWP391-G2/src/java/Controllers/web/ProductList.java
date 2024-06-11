c27340fa13508f7c1cfc837818b9cceba0f6a75e
PahmNogcHa2003 <pnha03dev.it@gmail.com>
2024-06-12 01:38:55 +0700

    Update

--- SWP391-G2/src/java/Controllers/web/ProductList.java
+++ SWP391-G2/src/java/Controllers/web/ProductList.java
@@ -2,49 +2,63 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
+package Controllers.web;
 
-package Controllers;
-
+import Dal.CategoriesDAO;
+import Dal.ProductsDAO;
+import Models.Categories;
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  *
- * @author pna29
+ * @author ROG
  */
-public class Search extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+public class ProductList extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Search</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Search at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+
+        ProductsDAO Pdao = new ProductsDAO();
+        CategoriesDAO Cdao = new CategoriesDAO();
+        List<Categories> listCate = Cdao.loadCategory();
+
+        String indexP = request.getParameter("index");
+        int index = 1;
+        if (indexP != null) {
+            index = Integer.parseInt(indexP);
         }
-    } 
+
+        int count = Pdao.getCount();
+        int endPage = count / 12;
+        if (count % 12 != 0) {
+            endPage++;
+        }
+        request.setAttribute("listProduct", Pdao.getPaging(index));
+        request.setAttribute("endP", endPage);
+        request.setAttribute("tag", index);
+        request.setAttribute("category", listCate);
+        request.getRequestDispatcher("productList.jsp").forward(request, response);
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -52,12 +66,13 @@ public class Search extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -65,12 +80,13 @@ public class Search extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
