c6c29fa7a16695a6579d5de20cc152a08e889e2d
Nguyen Quang Bich HE173220 <bichnqhe173220@fpt.edu.vn>
2024-06-14 00:58:54 +0700

    divide folder in view

--- SWP391-G2/src/java/Controllers/common/LoginGoogleHandler.java
+++ SWP391-G2/src/java/Controllers/common/LoginGoogleHandler.java
@@ -1,10 +1,11 @@
-package Controllers;
+package Controllers.common;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import Controllers.Constants;
 import Dal.AccountsDAO;
 import Models.Accounts;
 
