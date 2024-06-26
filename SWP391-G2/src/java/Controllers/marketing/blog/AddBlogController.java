08726aace4843621a63b1661184332c23ed0ffb6
ShaShalom <tokonise05171@fpt.edu.vn>
2024-06-26 14:40:21 +0700

    add anotation

--- SWP391-G2/src/java/Controllers/marketing/blog/AddBlogController.java
+++ SWP391-G2/src/java/Controllers/marketing/blog/AddBlogController.java
@@ -8,6 +8,7 @@ import Dal.BlogDAO;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
