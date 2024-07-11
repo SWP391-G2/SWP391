094e5f5dc6e40961c2cb3182ed814c8844568690
ShaShalom <tokonise05171@fpt.edu.vn>
2024-07-11 10:12:41 +0700

    fix authen formInforBlog

--- SWP391-G2/src/java/Controllers/marketing/blog/FormInforBlog.java
+++ SWP391-G2/src/java/Controllers/marketing/blog/FormInforBlog.java
@@ -5,6 +5,7 @@
 package Controllers.marketing.blog;
 
 import Dal.BlogDAO;
+import Models.Accounts;
 import Models.BlogResponseDTO;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
@@ -12,6 +13,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 /**
  *
@@ -23,6 +25,16 @@ public class FormInforBlog extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+
+        Accounts account = (Accounts) session.getAttribute("account");
+
+        if (account == null || account.getRoleID() != 3) {
+            response.sendRedirect("login");
+            return;
+        }
+
         int idBlog = Integer.parseInt(request.getParameter("id"));
         BlogDAO blogDAO = new BlogDAO();
         BlogResponseDTO blog = blogDAO.findBlogById(idBlog);
